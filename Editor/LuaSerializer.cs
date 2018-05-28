c037ce1c6f2c27b51037f98e541e528bd73581de
GuoXiaoBo <ambitiongxb@foxmail.com>
2018-05-28 18:27:48 +0800

    修改 Ask repeated

--- Editor/LuaSerializer.cs
+++ Editor/LuaSerializer.cs
@@ -214,7 +214,11 @@
                                 {
                                     foreach (var v in ds.fieldItem)
                                     {
-                                        referencesStruct += "\tPB." + descriptor.FieldName + "." + v.FieldName + " = " + descriptor.FieldName + "." + v.FieldName + "\r\n";
+                                        //referencesStruct += "\tPB." + descriptor.FieldName + "." + v.FieldName + " = " + descriptor.FieldName + "." + v.FieldName + "\r\n";
+
+                                        referencesStruct += "\tfor i = 1, #" + descriptor.FieldName + "." + v.FieldName + " do\r\n";
+                                        referencesStruct += "\t\ttable.insert( PB." + descriptor.FieldName + "." + v.FieldName + "," + descriptor.FieldName + "." + v.FieldName + "[i]\r\n";
+                                        referencesStruct += "\tend\r\n";
                                     }
                                 }
                                 str = str11 + referencesStruct;
